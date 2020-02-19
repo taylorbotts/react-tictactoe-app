4eb1f46ea8850783bb556f1fc548a687d2b816e8
Taylor <taylorbotts@knights.ucf.edu>
2020-02-19 11:57:20 -0500

    resolves #1

--- src/App.js
+++ src/App.js
@@ -1,26 +1,62 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Game from './components/TicTacToe/index.js'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class Login extends React.Component {
+
+  clickHandler () {
+    console.log('User is logging in')
+    console.log(this)
+    console.log(this.refs.username.value)
+    for (const user of this.props.users) {
+      if (this.refs.username.value === user.username 
+          && this.refs.password.value === user.password) {
+            this.props.logInUser()
+      } else {
+        this.refs.username.value = ''
+        this.refs.password.value = ''
+      }
+    }
+  }
+
+  render () {
+    return (
+      <div>
+        <input ref='username' type='text'/>
+        <input ref='password' type='password'/>
+        <input type='button' value='Login' onClick={() => this.clickHandler()}/>
+      </div>
+    )
+  }
 }
 
-export default App;
+class App extends React.Component {
+
+  constructor (props) {
+    super (props)
+    this.state = {loggedIn:false,
+                  users:[{username:'test',
+                          password:'test'}]}
+  }
+
+  logInUser (loggedInStatus) {
+    this.setState({loggedIn:loggedInStatus})
+  }
+
+  render(){
+    let returnComponent
+    const login = <Login users={this.state.users} 
+                         logInUser={() => this.logInUser(true)}
+                  />
+    const game = <Game logOutUser={() => this.logInUser(false)}/>
+    if (!this.state.loggedIn) {
+      returnComponent = login
+    } else {
+      returnComponent = game
+    }
+    return returnComponent
+  }
+}
+
+export default App
